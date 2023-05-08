44dd9d52c29dc4da63a8f6f12fe575024263225f
Crowdbotics <team@crowdbotics.com>
2023-05-08 20:44:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-math-40557",
+  url: "https://steep-math-40557.botics.co",
+  api: "https://steep-math-40557.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
